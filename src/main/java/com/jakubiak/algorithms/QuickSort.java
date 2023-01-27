819f6898247a14bac08d0f48bb9f6da281c4b592
jjfork <jakubiakrjulian@gmail.com>
2023-01-27 20:28:40 +0100

    QuickSort changed to iterative

--- src/main/java/com/jakubiak/algorithms/QuickSort.java
+++ src/main/java/com/jakubiak/algorithms/QuickSort.java
@@ -1,33 +1,29 @@
 package com.jakubiak.algorithms;
 
+import java.util.Stack;
+
 public class QuickSort implements SortingAlgorithm {
-    @Override
-    public  int[] sort(int[] arr) {
-        sort(arr, 0, arr.length - 1);
-    return arr;
-}
 
-    private  void sort(int[] arr, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-
-        int pivotIndex = partition(arr, left, right);
-        sort(arr, left, pivotIndex - 1);
-        sort(arr, pivotIndex + 1, right);
-    }
-
-    private static int partition(int[] arr, int left, int right) {
-        int pivotValue = arr[right];
-        int pivotIndex = left;
-        for (int i = left; i < right; i++) {
-            if (arr[i] < pivotValue) {
-                swap(arr, i, pivotIndex);
-                pivotIndex++;
+    private static int partition(int[] input, int position, int start, int end) {
+        int l = start;
+        int h = end - 2;
+        int piv = input[position];
+        swap(input, position, end - 1);
+        while (l < h) {
+            if (input[l] < piv) {
+                l++;
+            } else if (input[h] >= piv) {
+                h--;
+            } else {
+                swap(input, l, h);
             }
         }
-        swap(arr, pivotIndex, right);
-        return pivotIndex;
+        int idx = h;
+        if (input[h] < piv) {
+            idx++;
+        }
+        swap(input, end - 1, idx);
+        return idx;
     }
 
     private static void swap(int[] arr, int i, int j) {
@@ -36,8 +32,32 @@ public class QuickSort implements SortingAlgorithm {
         arr[j] = temp;
     }
 
+
+    @Override
+    public int[] sort(int[] arr) {
+        Stack<Integer> stack = new Stack();
+        stack.push(0);
+        stack.push(arr.length);
+        while (!stack.isEmpty()) {
+            int end = stack.pop();
+            int start = stack.pop();
+            if (end - start < 2) {
+                continue;
+            }
+            int p = start + ((end - start) / 2);
+            p = partition(arr, p, start, end);
+            stack.push(p + 1);
+            stack.push(end);
+            stack.push(start);
+            stack.push(p);
+        }
+        return arr;
+    }
+
     @Override
     public String getAlgorithmName() {
         return QuickSort.class.getName();
     }
+
+
 }
